4b149df242033d75d7353474b4be5efc7cf25783
Steven Zheng <szheng.persevere@gmail.com>
2024-01-19 12:49:10 -0600

    finished single d6

--- main.js
+++ main.js
@@ -35,10 +35,20 @@ const sortByNumber = function(arr) {
 /*******************
  * EVENT LISTENERS *
  *******************/
+let sixroll = document.querySelector("#d6-roll")
+sixroll.addEventListener("click",()=>{
+  sixes.push(getRandomNumber(6))
+  document.getElementById("d6-roll").src = `./images/d6/${sixes[sixes.length-1]}.png`
+  let sixmean = mean(sixes)
+  document.querySelector("#d6-rolls-mean").innerHTML = `${sixmean}`
+  let sixmedian = median(sortByNumber(sixes))
+  document.querySelector("#d6-rolls-median").innerHTML = `${sixmedian}`
+  let sixmode = mode(sortByNumber(sixes))
+  document.querySelector("#d6-rolls-mode").innerHTML = `${sixmode}`
+})
 
 
-
-
+// images/d6/1.png
 
 /******************
  * RESET FUNCTION *
@@ -55,3 +65,41 @@ const sortByNumber = function(arr) {
 /****************
  * MATH SECTION *
  ****************/
+const mean = function(arr){
+  let total = 0
+  for(let i = 0;i < arr.length; i++){
+    total = total + arr[i]
+  }
+  return (total / arr.length).toFixed(3)
+}
+
+const median = function(arr){
+  if(arr.length === 0){
+  }else if(arr.length % 2 === 0){
+    return (arr[arr.length/2 - 1] + arr[arr.length/2]) / 2
+  } else {
+    return arr[arr.length / 2 -.5] 
+  }
+}
+
+const mode = function(arr){
+  let obj = {}
+  for(let key of arr){
+    if(obj[key] === undefined){
+      obj[key] = 1
+    } else {
+      obj[key]++
+    }
+  }
+  let highestarr = []
+  let highest = 0
+  for(let key in obj){
+    if(obj[key]=== highest){
+      highestarr.push(key)
+    } else if(obj[key] > highest){
+      highest = obj[key]
+      highestarr = [key]
+    }
+  }
+  return highestarr.join(", ")
+}
