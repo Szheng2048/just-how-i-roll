3687ccf79116d12ba834436fc6bc9b7e4c2cd9cc
Steven Zheng <szheng.persevere@gmail.com>
2024-01-22 10:20:23 -0600

    finished rough draft

--- main.js
+++ main.js
@@ -29,7 +29,11 @@ const sortByNumber = function(arr) {
 /*******************
  * YOUR CODE BELOW *
  *******************/
-
+const doublesixroll = function(num1,num2,arr,doublearr){
+   arr.push(num1+num2)
+   doublearr.push(num1,num2)
+   
+}
 
 
 /*******************
@@ -47,13 +51,75 @@ sixroll.addEventListener("click",()=>{
   document.querySelector("#d6-rolls-mode").innerHTML = `${sixmode}`
 })
 
+let d1sixroll = document.querySelector("#double-d6-roll-1")
+let d2sixroll = document.querySelector("#double-d6-roll-2")
+let dtotal = []
+let dseperate = []
+d1sixroll.addEventListener("click",()=>{
+  doublesixroll(getRandomNumber(6),getRandomNumber(6),dtotal,dseperate)
+  document.getElementById("double-d6-roll-1").src = `./images/d6/${dseperate[dseperate.length-2]}.png`
+  document.getElementById("double-d6-roll-2").src = `./images/d6/${dseperate[dseperate.length-1]}.png`
+  document.querySelector("#double-d6-rolls-mean").innerHTML = `${mean(dtotal)}`
+  document.querySelector("#double-d6-rolls-median").innerHTML = `${median(sortByNumber(dtotal))}`
+  document.querySelector("#double-d6-rolls-mode").innerHTML = `${mode(dtotal)}`
+  console.log(dseperate)
+})
+d2sixroll.addEventListener("click",()=>{
+  doublesixroll(getRandomNumber(6),getRandomNumber(6),dtotal,dseperate)
+  document.getElementById("double-d6-roll-1").src = `./images/d6/${dseperate[dseperate.length-2]}.png`
+  document.getElementById("double-d6-roll-2").src = `./images/d6/${dseperate[dseperate.length-1]}.png`
+  document.querySelector("#double-d6-rolls-mean").innerHTML = `${mean(dtotal)}`
+  document.querySelector("#double-d6-rolls-median").innerHTML = `${median(sortByNumber(dtotal))}`
+  document.querySelector("#double-d6-rolls-mode").innerHTML = `${mode(dtotal)}`
+  console.log(dseperate)
+})
 
-// images/d6/1.png
+let d12roll = document.querySelector("#d12-roll")
+d12roll.addEventListener("click",()=>{
+   twelves.push(getRandomNumber(12))
+   document.getElementById("d12-roll").src = `images/numbers/${twelves[twelves.length-1]}.png`
+   document.getElementById("d12-rolls-mean").innerHTML = `${mean(twelves)}`
+   document.getElementById("d12-rolls-median").innerHTML = `${median(twelves)}`
+   document.getElementById("d12-rolls-mode").innerHTML = `${mode(twelves)}`
+})
+
+let d20roll = document.querySelector("#d20-roll")
+d20roll.addEventListener("click",()=>{
+  twenties.push(getRandomNumber(20))
+  document.getElementById("d20-roll").src = `images/numbers/${twenties[twenties.length-1]}.png`
+  document.getElementById("d20-rolls-mean").innerHTML = `${mean(twenties)}`
+  document.getElementById("d20-rolls-median").innerHTML = `${median(twenties)}`
+  document.getElementById("d20-rolls-mode").innerHTML = `${mode(twenties)}`
+})
 
 /******************
  * RESET FUNCTION *
  ******************/
-
+document.querySelector("#reset-button").addEventListener("click",()=>{
+  sixes.length = 0
+  doubleSixes.length = 0
+  twelves.length = 0
+  twenties.length = 0
+  dseperate.length = 0
+  dtotal.length = 0
+  document.getElementById("d6-roll").src = "./images/start/d6.png"
+  document.getElementById("double-d6-roll-1").src = "./images/start/d6.png"
+  document.getElementById("double-d6-roll-2").src = "./images/start/d6.png"
+  document.getElementById("d12-roll").src = "./images/start/d12.jpeg"
+  document.getElementById("d20-roll").src = "./images/start/d20.jpg"
+  document.getElementById("d6-rolls-mean").innerHTML = ""
+  document.getElementById("d6-rolls-median").innerHTML = ""
+  document.getElementById("d6-rolls-mode").innerHTML = ""
+  document.getElementById("double-d6-rolls-mean").innerHTML = ""
+  document.getElementById("double-d6-rolls-median").innerHTML = ""
+  document.getElementById("double-d6-rolls-mode").innerHTML = ""
+  document.getElementById("d12-rolls-mean").innerHTML = ""
+  document.getElementById("d12-rolls-median").innerHTML = ""
+  document.getElementById("d12-rolls-mode").innerHTML = ""
+  document.getElementById("d20-rolls-mean").innerHTML = ""
+  document.getElementById("d20-rolls-median").innerHTML = ""
+  document.getElementById("d20-rolls-mode").innerHTML = ""
+})
 
 
 /****************************
